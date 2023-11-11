5fb3e103b1945be70ca91735fe41cc07d286db7a
kharisma tara mukti <kharismataram@gmail.com>
2023-11-12 06:32:40 +0700

    model dan migrasion dari user dan product

---
+++ database/migrations/2023_11_11_232836_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\Expr\Cast\String_;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->String('name');
+            $table->bigInteger('price');
+            $table->longText('description');
+            $table->String('slug')->unique();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
